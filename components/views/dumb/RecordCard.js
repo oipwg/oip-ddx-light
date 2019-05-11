18724e940525eb29f6379b5837d8484b86a01e16
orpheus <ryanachacon@gmail.com>
2019-05-11 09:22:39 -0700

    super basic template/record card ui

--- components/views/dumb/RecordCard.js
+++ components/views/dumb/RecordCard.js
@@ -2,6 +2,9 @@ import React from 'react'
 import withStyles from 'react-jss'
 import PropTypes from 'prop-types'
 
+const BASIC_TEMPLATE = 'tmpl_00000000000BA51C'
+const FILE_TEMPLATE = 'tmpl_000000000000F113'
+
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -13,12 +16,46 @@ const styles = theme => ({
     boxShadow: theme.shadows[1],
     borderRadius: '3px',
     boxSizing: 'border-box'
+  },
+  recordSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    flex: '0 0 56px',
+    position: 'relative'
+  },
+  recordFieldTitle: {
+    position: 'absolute',
+    fontSize: 12,
+    color: 'grey',
+    top: 0,
+    left: 0
+  },
+  recordField: {
+    fontSize: 14,
+    marginTop: 20
   }
 })
 
 const RecordCard = ({ classes, record, meta }) => {
-  return <div className={classes.root}>
+  const { details } = record
+  const { [BASIC_TEMPLATE]: basicTemplate } = details
 
+  const { title, description, year } = basicTemplate
+
+  return <div className={classes.root}>
+    <div className={classes.recordSection}>
+      <span className={classes.recordFieldTitle}>Title</span>
+      <span className={classes.recordField}>{title}</span>
+    </div>
+    <div className={classes.recordSection}>
+      <span className={classes.recordFieldTitle}>Year</span>
+      <span className={classes.recordField}>{year}</span>
+    </div>
+    <div className={classes.recordSection}>
+      <span className={classes.recordFieldTitle}>Description</span>
+      <span className={classes.recordField}>{description}</span>
+    </div>
   </div>
 }
 
--- components/views/dumb/TemplateCard.js
+++ components/views/dumb/TemplateCard.js
@@ -13,12 +13,65 @@ const styles = theme => ({
     boxShadow: theme.shadows[1],
     borderRadius: '3px',
     boxSizing: 'border-box'
+  },
+  templateField: {
+    display: 'flex',
+    flex: 1,
+    position: 'relative',
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  fieldName: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    fontSize: 12,
+    color: 'grey'
+  },
+  fieldData: {
+    margin: 0,
+    fontSize: 12
+  },
+  templateActions: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1
   }
 })
 
-const TemplateCard = ({ classes, record, meta }) => {
+const TemplateCard = ({ classes, template, meta }) => {
   return <div className={classes.root}>
+    <div className={classes.templateField}>
+      <span className={classes.fieldName}>
+        Friendly Name
+      </span>
+      <p className={classes.fieldData}>{template.friendly_name}</p>
+    </div>
 
+    <div className={classes.templateField}>
+      <span className={classes.fieldName}>
+        Name
+      </span>
+      <p className={classes.fieldData}>{template.name}</p>
+    </div>
+
+    <div className={classes.templateField}>
+      <span className={classes.fieldName}>
+        Identifier
+      </span>
+      <p className={classes.fieldData}>{template.identifier}</p>
+    </div>
+
+    <div className={classes.templateField}>
+      <span className={classes.fieldName}>
+        Description
+      </span>
+      <p className={classes.fieldData}>{template.description}</p>
+    </div>
+
+    <div className={classes.templateActions}>
+
+    </div>
   </div>
 }
 
