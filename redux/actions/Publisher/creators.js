bb55c996af1533d93cc9bf97e7ec126c81b33317
orpheus <ryanachacon@gmail.com>
2019-05-13 12:07:15 -0700

    add publisher actions/reducer

--- redux/actions/Publisher/creators.js
+++ redux/actions/Publisher/creators.js
@@ -1,8 +1,41 @@
-export const RECORD = 'RECORD'
-export const TEMPLATE = 'TEMPLATE'
+export const OPEN_PUBLISHER_MODAL = 'OPEN_PUBLISHER_MODAL'
+export const openPublisherModal = {
+  type: OPEN_PUBLISHER_MODAL
+}
 
-export const SET_PUBLISH_TYPE = 'SET_PUBLISH_TYPE'
-export const setPublishType = (publishType) => ({
-  type: SET_PUBLISH_TYPE,
-  publishType
+export const CLOSE_PUBLISHER_MODAL = 'CLOSE_PUBLISHER_MODAL'
+export const closePublisherModal = {
+  type: CLOSE_PUBLISHER_MODAL
+}
+
+export const PUBLISH_RECORD = 'PUBLISH_RECORD'
+export const publishRecord = {
+  type: PUBLISH_RECORD
+}
+
+export const PUBLISH_TEMPLATE = 'PUBLISH_TEMPLATE'
+export const publishTemplate = {
+  type: PUBLISH_TEMPLATE
+}
+
+export const FORK_TEMPLATE = 'FORK_TEMPLATE'
+export const forkTemplate = descriptorSetProto => ({
+  type: FORK_TEMPLATE,
+  descriptorSetProto
+})
+
+export const PUBLISH_SUCCESS = 'PUBLISH_SUCCESS'
+export const publishSuccess = {
+  type: PUBLISH_SUCCESS
+}
+
+export const PUBLISH_PENDING = 'PUBLISH_PENDING'
+export const publishPending = {
+  type: PUBLISH_PENDING
+}
+
+export const PUBLISH_ERROR = 'PUBLISH_ERROR'
+export const publishError = (error) => ({
+  type: PUBLISH_ERROR,
+  error
 })
--- redux/reducers/Publisher.js
+++ redux/reducers/Publisher.js
@@ -1,15 +1,66 @@
 import * as actions from '../actions/Publisher/creators'
 
+const RECORD = 'record'
+
+const TEMPLATE = 'TEMPLATE'
+
 function Publisher (state = {
-  publishType: actions.RECORD,
-  publishRecord: undefined,
-  publishTemplate: undefined
+  openPublisherModal: false,
+  publishType: RECORD,
+  publishSuccess: false,
+  publishPending: false,
+  publishError: false,
+  publishErrorMessage: null
 }, action) {
   switch (action.type) {
-    case actions.SET_PUBLISH_TYPE:
+    case actions.OPEN_PUBLISHER_MODAL:
       return {
         ...state,
-        publishType: action.publishType
+        openPublisherModal: true
+      }
+    case actions.CLOSE_PUBLISHER_MODAL:
+      return {
+        ...state,
+        openPublisherModal: false
+      }
+    case actions.PUBLISH_RECORD:
+      return {
+        ...state,
+        publishType: RECORD
+      }
+    case actions.PUBLISH_TEMPLATE:
+      return {
+        ...state,
+        publishType: TEMPLATE
+      }
+    case actions.FORK_TEMPLATE:
+      return {
+        ...state,
+        forkedTemplate: action.descriptorSetProto
+      }
+    case actions.PUBLISH_SUCCESS:
+      return {
+        ...state,
+        publishSuccess: true,
+        publishError: false,
+        publishPending: false,
+        publishErrorMessage: null
+      }
+    case actions.PUBLISH_PENDING:
+      return {
+        ...state,
+        publishSuccess: false,
+        publishError: false,
+        publishPending: true,
+        publishErrorMessage: null
+      }
+    case actions.PUBLISH_ERROR:
+      return {
+        ...state,
+        publishSuccess: false,
+        publishError: true,
+        publishPending: false,
+        publishErrorMessage: action.error
       }
     default:
       return state
