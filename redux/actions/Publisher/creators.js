0456ec2cf0edb6c929cc1c46a6d9081abb1c0c33
orpheus <ryanachacon@gmail.com>
2019-05-13 14:44:42 -0700

    turn actions into fns

--- redux/actions/Publisher/creators.js
+++ redux/actions/Publisher/creators.js
@@ -1,22 +1,22 @@
 export const OPEN_PUBLISHER_MODAL = 'OPEN_PUBLISHER_MODAL'
-export const openPublisherModal = {
+export const openPublisherModal = () => ({
   type: OPEN_PUBLISHER_MODAL
-}
+})
 
 export const CLOSE_PUBLISHER_MODAL = 'CLOSE_PUBLISHER_MODAL'
-export const closePublisherModal = {
+export const closePublisherModal = () => ({
   type: CLOSE_PUBLISHER_MODAL
-}
+})
 
 export const PUBLISH_RECORD = 'PUBLISH_RECORD'
-export const publishRecord = {
+export const publishRecord = () => ({
   type: PUBLISH_RECORD
-}
+})
 
 export const PUBLISH_TEMPLATE = 'PUBLISH_TEMPLATE'
-export const publishTemplate = {
+export const publishTemplate = () => ({
   type: PUBLISH_TEMPLATE
-}
+})
 
 export const FORK_TEMPLATE = 'FORK_TEMPLATE'
 export const forkTemplate = descriptorSetProto => ({
@@ -25,14 +25,14 @@ export const forkTemplate = descriptorSetProto => ({
 })
 
 export const PUBLISH_SUCCESS = 'PUBLISH_SUCCESS'
-export const publishSuccess = {
+export const publishSuccess = () => ({
   type: PUBLISH_SUCCESS
-}
+})
 
 export const PUBLISH_PENDING = 'PUBLISH_PENDING'
-export const publishPending = {
+export const publishPending = () => ({
   type: PUBLISH_PENDING
-}
+})
 
 export const PUBLISH_ERROR = 'PUBLISH_ERROR'
 export const publishError = (error) => ({
