f7b875357d444973bc712f0ad3ad033ca6885b1b
orpheus <ryanachacon@gmail.com>
2019-06-06 20:01:58 -0700

    wrap next config with css loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js is not transformed by Babel. So you can only use javascript features supported by your version of Node.js.
+const withCSS = require('@zeit/next-css')
 
-module.exports = {
+module.exports = withCSS({
   webpack: (config) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
@@ -15,4 +16,4 @@ module.exports = {
     // Important: return the modified config
     return config
   }
-}
+})
