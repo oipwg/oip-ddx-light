63cba117e9964b8285dea1eda1582b34f5b3558d
orpheus <ryanachacon@gmail.com>
2019-06-11 08:04:10 -0700

    add comments for config variables

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
 const config = {
+  // this is the enpoint the api will use to fetch records/templates. ex. a running oipd node
   daemonApiUrl: 'https://api.oip.io/oip',
-  templateFilter: [],
-  templateOperand: 'OR',
-  privatekey: 'RDpg84jEJWeC8YFNSWk32ypuZKhArfPbxXELToKeSvzAxUoaaFFh',
-  network: 'mainnet',
+  // a url to a running Insight explorer endpoint
   explorerUrl: 'https://livenet.flocha.in/api',
-  primaryColor: '#3688aa',
-  secondaryColor: '#d8443a',
+  // testnet will not offer full functionality. but if you're using this you can probably figure out how to add what you want
+  network: 'mainnet',
+  // an array of string template names (tmpl_XXXXXXXX) to filter
+  templateFilter: [],
+  // whether you want to filter for records with (tmpl_A AND) tmpl_b or (tmpl_A OR tmpl_B)
+  templateOperand: 'OR',
+  // this is your WIF, your Wallet Import Format. Google for more information. If using flo-qt, use command: dumpprivkey
+  privatekey: 'RDpg84jEJWeC8YFNSWk32ypuZKhArfPbxXELToKeSvzAxUoaaFFh',
+  // this is the txid to a record containing a registerd platform template. needed and used for platform cuts
   platformRegistrationTxid: '7b4f590cc82d3a023e5842ab1c9395a216551d085918158fae19a2b4138fe106',
-  showOnlyVerifiedPublishers: false
+  // this sets the default value for whether you want to show only verified publishers or not
+  showOnlyVerifiedPublishers: false,
+  // theme information. can set your primary and secondary color here. Two most used theme variables
+  primaryColor: '#3688aa',
+  secondaryColor: '#d8443a'
 }
 
 export default config
