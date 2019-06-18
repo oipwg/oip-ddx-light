fca3ec640292ec081856781089a2a0770075b4c6
orpheus <ryanachacon@gmail.com>
2019-06-18 12:48:41 -0700

    remove trailing slash for ipfs gateway in config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   // a url to a running Insight explorer endpoint
   explorerUrl: 'https://livenet.flocha.in/api',
   // ipfs gateway url
-  ipfsGatewayUrl: 'https://ipfs.io/ipfs/',
+  ipfsGatewayUrl: 'https://ipfs.io/ipfs',
   // testnet will not offer full functionality. but if you're using this you can probably figure out how to add what you want
   network: 'mainnet',
   // an array of string template names (tmpl_XXXXXXXX) to filter
--- helpers/getIpfsUrl.js
+++ helpers/getIpfsUrl.js
@@ -3,7 +3,7 @@ import config from '../config'
 const ipfsGateway = config.ipfsGatewayUrl
 
 const getIpfsUrl = ({ dirName, filename }) => {
-  return `${ipfsGateway}${dirName}/${filename}`
+  return `${ipfsGateway}/${dirName}/${filename}`
 }
 
 export default getIpfsUrl
