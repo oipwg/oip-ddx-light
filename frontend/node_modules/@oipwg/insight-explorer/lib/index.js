3eb3b9d408c3ec408084f3bb224f64031c932085
truedev08 <martin.adams08@yahoo.com>
2021-04-14 16:42:51 -0500

    removed node_modules

--- frontend/node_modules/@oipwg/insight-explorer/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _Insight = _interopRequireDefault(require("./Insight"));
-
-module.exports = {
-  Insight: _Insight["default"]
-};
